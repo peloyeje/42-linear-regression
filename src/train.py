c0cafe273f2d316d199ea53ff55a97988f2102e8
Sami Mhirech <samimhirech@MacBook-Air-de-Sami.local>
2018-10-03 18:49:36 +0200

    Scaled the price data

--- src/train.py
+++ src/train.py
@@ -4,13 +4,17 @@ import matplotlib.pyplot as plt
 
 class LinearRegression(object):
     def __init__(self, learning_rate, iterations):
-        data = np.genfromtxt('./data/data.csv', delimiter=',', skip_header=1)
+        data = np.genfromtxt('../data/data.csv', delimiter=',', skip_header=1)
         self.price = data[:, 1]
-
         self.kms = data[:, 0]
+
         self.mean_kms = np.mean(self.kms)
         self.std_kms = np.std(self.kms)
         self.norm_kms = (self.kms - self.mean_kms)/self.std_kms
+
+        self.mean_price = np.mean(self.price)
+        self.std_price = np.std(self.price)
+        self.norm_price = (self.price - self.mean_price)/self.std_price
 
         self.theta0 = 0
         self.theta1 = 0
@@ -26,8 +30,8 @@ class LinearRegression(object):
 
     def new_thetas(self, theta0, theta1, kms):
         vfunc = self.vec_estimatePrice(theta0, theta1)
-        theta0_grad = 2 * np.mean(vfunc(kms) - self.price)
-        theta1_grad = 2  * np.mean((vfunc(kms) - self.price) * kms)
+        theta0_grad = 2 * np.mean(vfunc(kms) - self.norm_price)
+        theta1_grad = 2  * np.mean((vfunc(kms) - self.norm_price) * kms)
         theta0 = theta0 - theta0_grad * self.learning_rate
         theta1 = theta1 - theta1_grad * self.learning_rate
         return theta0, theta1
@@ -40,11 +44,11 @@ class LinearRegression(object):
             all_theta0 = np.append(all_theta0, np.array([[self.theta0]]), axis = 0)
             all_theta1 = np.append(all_theta1, np.array([[self.theta1]]), axis = 0)
 
-        self.all_theta0 = all_theta0 - (self.mean_kms/self.std_kms)
-        self.all_theta1 = all_theta1/self.std_kms
+        self.all_theta0 = self.std_price * all_theta0 - all_theta1 * self.mean_kms * (self.std_price / self.std_kms) + self.mean_price
+        self.all_theta1 = all_theta1* (self.std_price / self.std_kms)
 
         if verbose == True:
-            np.savetxt('./output/thetas.txt', [self.all_theta0[-1], self.all_theta1[-1]], delimiter=',')
+            np.savetxt('../output/thetas.txt', [self.all_theta0[-1], self.all_theta1[-1]], delimiter=',')
             print("The thetas have been uploaded into ../output/thetas.txt")
 
     def plot_loss(self):
@@ -54,6 +58,8 @@ class LinearRegression(object):
         plt.plot(loss)
         plt.show()
 
-    def plot_linear(self):
-        self.train(verbose=False)
-        theta0, theta1 = self.all_theta0[-1], self.all_theta1[-1]
+        return loss
+
+    #def plot_linear(self):
+    #    self.train(verbose=False)
+    #    theta0, theta1 = self.all_theta0[-1], self.all_theta1[-1]
